03e4043c472fb84d1da01e571d5da190a1b5cd43
Vamsi Ravula <codingsaduvu@gmail.com>
2025-05-16 17:40:22 -0700

    Add token creation to OpenShift Script

--- .ansible/demo_scripts/second_openshift_setup.sh
+++ .ansible/demo_scripts/second_openshift_setup.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
 oc apply -f https://raw.githubusercontent.com/rpscodes/app_connectivity_summit25/refs/heads/main/.ansible/demo_scripts/openshift_site.yaml
+echo "Waiting 50 seconds for site to be up"
 sleep 50
 
 oc apply -f https://raw.githubusercontent.com/rpscodes/app_connectivity_summit25/refs/heads/main/.ansible/demo_scripts/listener.yaml
+echo "Waiting 20 seconds for listener to be created"
 sleep 20
 
 oc apply -f https://raw.githubusercontent.com/rpscodes/app_connectivity_summit25/refs/heads/main/.ansible/demo_scripts/grant.yaml
+echo "Waiting 20 seconds for grant to be created"
 sleep 20
 
 oc apply -f https://raw.githubusercontent.com/app-connectivity-workshop/scripts/refs/heads/main/m1/network_console_deploy.yaml -n travel-db
+echo "Waiting 20 seconds for console to be created"
+sleep 20
+
+curl -s https://raw.githubusercontent.com/app-connectivity-workshop/scripts/refs/heads/main/m1/convert_grant_token.sh | bash > summit_token.yaml
+echo "Waiting 20 seconds for token to be created"
 sleep 20
 
 oc label namespace travel-agency istio-injection=enabled --overwrite=true
-sleep 30
+echo "Waiting 20 seconds for namespace to be created"
+sleep 20
 
 
 
 
 oc get deployments -n travel-agency -o name \
   | xargs -I{} oc rollout -n travel-agency restart {}
-sleep 60
+
+echo "Waiting 10 seconds for deployments to be rolled out"
+sleep 10
 
 
 
 watch oc get pods -n travel-agency
+
+echo "Press ctrl+c to if you see 2 pods per deployment"
+
 
--- .ansible/demo_scripts/third_token_transfer.sh
+++
@@ -1,4 +0,0 @@
-curl -s https://raw.githubusercontent.com/app-connectivity-workshop/scripts/refs/heads/main/m1/convert_grant_token.sh | bash > summit_token.yaml
-sleep 20
-
-scp summit_token.yaml lab-user@rhel.wzwnh-1.sandbox4735.opentlc.com:/home/lab-user/.local/share/skupper/namespaces/default/input/resources/
